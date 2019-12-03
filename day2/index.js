b547ff3314d611f42974cdcdfd4df1a29e1dfaf5
Cameron Pittman <cameron.w.pittman@gmail.com>
2019-12-02 22:41:00 -0500

    add day 2 part 2 solution

--- day2/index.js
+++ day2/index.js
@@ -8,13 +8,36 @@ fs.readFile("./day2/data.txt", function (err, data) {
         console.error(err);
         process.exit(1);
     }
-    var opcodes = data
+    var originalOpcodes = data
         .toString()
         .split(",")
         .map(function (l) { return +l; });
+    var part1 = originalOpcodes.slice();
     // the question wants us to do these replacements first to fix the 1202
-    opcodes[1] = 12;
-    opcodes[2] = 2;
-    opcodes = compute_1["default"](opcodes);
-    console.log(opcodes[0]);
+    part1[1] = 12;
+    part1[2] = 2;
+    part1 = compute_1["default"](part1);
+    console.log("part 1:", part1[0]);
+    var found = false;
+    for (var i = 0; i < 99; i++) {
+        if (found) {
+            break;
+        }
+        for (var j = 0; j < 99; j++) {
+            if (found) {
+                break;
+            }
+            var part2 = originalOpcodes.slice();
+            part2[1] = i;
+            part2[2] = j;
+            part2 = compute_1["default"](part2);
+            if (part2[0] === 19690720) {
+                console.log("part2:", 100 * part2[1] + part2[2]);
+                found = true;
+            }
+        }
+    }
+    if (!found) {
+        console.log("failure :(");
+    }
 });
--- day2/index.ts
+++ day2/index.ts
@@ -9,14 +9,44 @@ fs.readFile("./day2/data.txt", (err, data) => {
     process.exit(1);
   }
 
-  let opcodes: Array<number> = data
+  const originalOpcodes: Array<number> = data
     .toString()
     .split(",")
     .map(l => +l);
-  // the question wants us to do these replacements first to fix the 1202
-  opcodes[1] = 12;
-  opcodes[2] = 2;
 
-  opcodes = compute(opcodes);
-  console.log(opcodes[0]);
+  let part1 = originalOpcodes.slice();
+  // the question wants us to do these replacements first to fix the 1202
+  part1[1] = 12;
+  part1[2] = 2;
+
+  part1 = compute(part1);
+  console.log("part 1:", part1[0]);
+
+  // I'm so sorry
+  // taking a brute force search approach to part 2
+  let found = false;
+  for (let i = 0; i < 99; i++) {
+    if (found) {
+      break;
+    }
+
+    for (let j = 0; j < 99; j++) {
+      if (found) {
+        break;
+      }
+
+      let part2 = originalOpcodes.slice();
+      part2[1] = i;
+      part2[2] = j;
+      part2 = compute(part2);
+      if (part2[0] === 19690720) {
+        console.log("part2:", 100 * part2[1] + part2[2]);
+        found = true;
+      }
+    }
+  }
+
+  if (!found) {
+    console.log("failure :(");
+  }
 });
